1803687e0b512acbb8fc75e766a04142eb99c4fb
Incredible Srv <saurabh.vaish1993@gmail.com>
2018-12-28 22:38:58 +0530

    Update Solution.java

--- Java/java BigDecimal/Solution.java
+++ Java/java BigDecimal/Solution.java
@@ -2,7 +2,7 @@
 
 Java's BigDecimal class can handle arbitrary-precision signed decimal numbers. Let's test your knowledge of them!
 
-Given an array, , of  real number strings, sort them in descending order — but wait, there's more! Each number must be printed in the exact same format as it was read from stdin, meaning that  is printed as , and  is printed as . If two numbers represent numerically equivalent values (e.g., ), then they must be listed in the same order as they were received as input).
+Given an array, , of  real number strings, sort them in descending order â€” but wait, there's more! Each number must be printed in the exact same format as it was read from stdin, meaning that  is printed as , and  is printed as . If two numbers represent numerically equivalent values (e.g., ), then they must be listed in the same order as they were received as input).
 
 Complete the code in the unlocked section of the editor below. You must rearrange array 's elements according to the instructions above.
 
@@ -70,7 +70,7 @@ class Solution{
             public int compare(String t1, String t2) {
                 BigDecimal b1 = new BigDecimal(t1);   // string to BigDecimal obj. foramt 
                 BigDecimal b2 = new BigDecimal(t2);
-                return b1.compareTo(b2);  // comapare 1st obj with 2nd then return bigger.
+                return b1.compareTo(b2);  // comapare 1st obj with 2nd then return -1 , 0 ,1 .
             }
         }));
        
