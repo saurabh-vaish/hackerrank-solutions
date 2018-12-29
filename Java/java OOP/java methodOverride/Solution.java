77e50db8f8c3d6dad86075701ca4f699026bb282
Incredible Srv <saurabh.vaish1993@gmail.com>
2018-12-30 00:08:28 +0530

    Add files via upload
    oop problem method Overriding

---
+++ Java/java OOP/java methodOverride/Solution.java
@@ -0,0 +1,43 @@
+/*
+You are given a partially completed code in the editor. Modify the code so that the code prints the following text:
+
+Hello I am a motorcycle, I am a cycle with an engine.
+My ancestor is a cycle who is a vehicle with pedals.
+
+*/
+
+/*
+
+@ author : saurabh vaish
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		String temp=super.define_me(); //Fix this line
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
